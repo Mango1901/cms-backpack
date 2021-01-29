86df83c446bcb786927b627004bf45b4acea5cf5
Mango1901 <hieu.tuhai2001@gmail.com>
2021-01-29 16:56:26 +0700

    fixed bug

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -16,14 +16,14 @@ Route::group([
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
     Route::get('charts/weekly-users', 'Charts\WeeklyUsersChartController@response')->name('charts.weekly-users.index');
-    Route::get('posts/ajax-custom-fields-options', 'PostCrudControllerOperation@customFieldsOptions');
-    Route::get('posts/ajax-category-options', 'PostCrudControllerOperation@categoryOptions');
-    Route::get('posts/ajax-tag-options', 'PostCrudControllerOperation@tagOptions');
-    Route::crud('permission', 'PermissionCrudControllerOperation');
-    Route::crud('role', 'RoleCrudControllerOperation');
-    Route::crud('user', 'UserCrudControllerOperation');
-    Route::crud('category', 'CategoryCrudControllerOperation');
-    Route::crud('tag', 'TagCrudControllerOperation');
-    Route::crud('post', 'PostCrudControllerOperation');
+    Route::get('posts/ajax-custom-fields-options', 'PostCrudController@customFieldsOptions');
+    Route::get('posts/ajax-category-options', 'PostCrudController@categoryOptions');
+    Route::get('posts/ajax-tag-options', 'PostCrudController@tagOptions');
+    Route::crud('permission', 'PermissionCrudController');
+    Route::crud('role', 'RoleCrudController');
+    Route::crud('user', 'UserCrudController');
+    Route::crud('category', 'CategoryCrudController');
+    Route::crud('tag', 'TagCrudController');
+    Route::crud('post', 'PostCrudController');
     Route::get('charts/new-entries', 'Charts\NewEntriesChartController@response')->name('charts.new-entries.index');
 }); // this should be the absolute last line of this file
