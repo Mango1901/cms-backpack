d1dc3602fddfeb846b94ccf0360c43450b8b58bf
Mango1901 <hieu.tuhai2001@gmail.com>
2021-01-29 14:40:32 +0700

    add back up add-ons function

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -21,3 +21,5 @@
 
 
 {{--<li class="nav-item"><a class="nav-link" href="{{ backpack_url('elfinder') }}\"><i class="nav-icon la la-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>--}}
+
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('backup') }}'><i class='nav-icon la la-hdd-o'></i> Backups</a></li>
